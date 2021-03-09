c4d3a2b1d7b8eb728057b036cf0230117fd580aa
HubashovD <34233868+HubashovD@users.noreply.github.com>
2021-03-09 20:29:06 +0200

    update 1.1

---
+++ data/location.js
@@ -0,0 +1,14 @@
+var json_location = {
+"type": "FeatureCollection",
+"name": "location",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "name": "1. Локація" }, "geometry": { "type": "Point", "coordinates": [ 32.6096425, 46.6302687 ] } },
+{ "type": "Feature", "properties": { "id": 2, "name": "2. Лебедине озеро" }, "geometry": { "type": "Point", "coordinates": [ 32.6225423, 46.6411386 ] } },
+{ "type": "Feature", "properties": { "id": 3, "name": "3. Замок Діснейленд" }, "geometry": { "type": "Point", "coordinates": [ 32.6192708, 46.634793 ] } },
+{ "type": "Feature", "properties": { "id": 4, "name": "4. Локація біля костелу" }, "geometry": { "type": "Point", "coordinates": [ 32.6064188, 46.6306983 ] } },
+{ "type": "Feature", "properties": { "id": 5, "name": "5. Річморвокзал" }, "geometry": { "type": "Point", "coordinates": [ 32.611641, 46.6242901 ] } },
+{ "type": "Feature", "properties": { "id": 6, "name": "6. Басейн біля бібліотеки Гончара" }, "geometry": { "type": "Point", "coordinates": [ 32.6346169, 46.6371679 ] } },
+{ "type": "Feature", "properties": { "id": 7, "name": "7. Мостоперехід" }, "geometry": { "type": "Point", "coordinates": [ 32.6035973, 46.6564064 ] } }
+]
+}
